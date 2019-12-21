11b769b68df8f0f8fb7c61027bda1e7c9001346e
chetankumaguptha <chetan.simplyfying@gmail.com>
2019-12-21 12:38:09 +0530

    Page addes

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,107 +9,40 @@ import StatsBox from '../components/StatsBox';
 import customerData from '../data/customer-data';
 import HeroImage from '../svg/HeroImage';
 import SvgCharts from '../svg/SvgCharts';
+import Personimage  from "../images/Project.svg"
+import '../components/layout/layout.css'
 
 export default () => (
   <Layout>
-    <section className="pt-20 md:pt-40">
-      <div className="container mx-auto px-8 lg:flex">
-        <div className="text-center lg:text-left lg:w-1/2">
-          <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">
-            Main title of your landing page
+    <section className="md:pt-10 md:pl-20 ">
+      <div className="container mx-auto px-8 lg:flex flex flex-wrap">
+        <div className="text-center lg:text-left py-10 lg:w-1/2  ">
+          <h1 className="text-4xl lg:text-5xl py-10 xl:text-6xl font-bold leading-none">
+            Victor Carrizalez
           </h1>
-          <p className="text-xl lg:text-2xl mt-6 font-light">
+          <p className="text-xl lg:text-2xl  border-l-8 px-12 py-3 font-light">
             Free landing page template to promote your business startup and generate leads for the
             offered services
           </p>
-          <p className="mt-8 md:mt-12">
-            <Button size="lg">Get Started</Button>
-          </p>
-          <p className="mt-4 text-gray-600">Sed fermentum felis ut cursu</p>
         </div>
-        <div className="lg:w-1/2">
-          <HeroImage />
+        <div className="lg:w-1/2 sm:w-1 object-center px-8 sm:py-4  md:w-1/2 md:mx-32 lg:-mx-1">
+          <img src={Personimage}/>
         </div>
       </div>
     </section>
-    <section id="features" className="py-20 lg:pb-40 lg:pt-48">
-      <div className="container mx-auto text-center">
-        <h2 className="text-3xl lg:text-5xl font-semibold">Main Features</h2>
-        <div className="flex flex-col sm:flex-row sm:-mx-3 mt-12">
-          <div className="flex-1 px-3">
-            <Card className="mb-8">
-              <p className="font-semibold text-xl">Service One</p>
-              <p className="mt-4">
-                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
-                velna vitae auctor integer.
-              </p>
-            </Card>
-          </div>
-          <div className="flex-1 px-3">
-            <Card className="mb-8">
-              <p className="font-semibold text-xl">Service Two</p>
-              <p className="mt-4">
-                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
-                velna vitae auctor integer.
-              </p>
-            </Card>
-          </div>
-          <div className="flex-1 px-3">
-            <Card className="mb-8">
-              <p className="font-semibold text-xl">Service Three</p>
-              <p className="mt-4">
-                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
-                velna vitae auctor integer.
-              </p>
-            </Card>
-          </div>
+    <section id="features" className="">
+      <div className="w-auto bg-gray-700 top-auto py-16">
+          <ui className="flex justify-center flex-wrap">
+                <a href="#buyer" className="bg-white hover:bg-blue-700 text-black hover:text-white font-bold ml-4 py-2 px-6 mt-3 border border-blue-700
+              rounded"> BUYER  </a>
+              <a href="#seller" className="bg-white hover:bg-blue-700 text-black hover:text-white font-bold ml-4 py-2 px-6 mt-3 border border-blue-700 rounded">SELLER  </a>
+              <a href="#retaltor" className="bg-white hover:bg-blue-700 text-black hover:text-white font-bold py-2 ml-4 px-6 mt-3 border border-blue-700 rounded">REALTOR  </a>
+          </ui>
+          <p class="text-center justify-center py-6 px-12 sm:text-center md:text-center lg:text-center xl:text-center ...">Lorem ipsum dolor sit amet ...</p>
         </div>
-      </div>
     </section>
-    <SplitSection
-      id="services"
-      primarySlot={
-        <div className="lg:pr-32 xl:pr-48">
-          <h3 className="text-3xl font-semibold leading-tight">Market Analysis</h3>
-          <p className="mt-8 text-xl font-light leading-relaxed">
-            Our team of enthusiastic marketers will analyse and evaluate how your company stacks
-            against the closest competitors
-          </p>
-        </div>
-      }
-      secondarySlot={<SvgCharts />}
-    />
-    <SplitSection
-      reverseOrder
-      primarySlot={
-        <div className="lg:pl-32 xl:pl-48">
-          <h3 className="text-3xl font-semibold leading-tight">
-            Design And Plan Your Business Growth Steps
-          </h3>
-          <p className="mt-8 text-xl font-light leading-relaxed">
-            Once the market analysis process is completed our staff will search for opportunities
-            that are in reach
-          </p>
-        </div>
-      }
-      secondarySlot={<SvgCharts />}
-    />
-    <SplitSection
-      primarySlot={
-        <div className="lg:pr-32 xl:pr-48">
-          <h3 className="text-3xl font-semibold leading-tight">
-            Search For Performance Optimization
-          </h3>
-          <p className="mt-8 text-xl font-light leading-relaxed">
-            With all the information in place you will be presented with an action plan that your
-            company needs to follow
-          </p>
-        </div>
-      }
-      secondarySlot={<SvgCharts />}
-    />
-    <section id="stats" className="py-20 lg:pt-32">
-      <div className="container mx-auto text-center">
+    <section id="seller" className="py-20  bg-red-600 ">
+      <div className="container mx-auto text-center h-screen lg:pt-32">
         <LabelText className="text-gray-600">Our customers get results</LabelText>
         <div className="flex flex-col sm:flex-row mt-8 lg:px-24">
           <div className="w-full sm:w-1/3">
@@ -124,9 +57,9 @@ export default () => (
         </div>
       </div>
     </section>
-    <section id="testimonials" className="py-20 lg:py-40">
-      <div className="container mx-auto">
-        <LabelText className="mb-8 text-gray-600 text-center">What customers are saying</LabelText>
+    <section id="buyer" className=" py-20 bg-orange-600">
+      <div className="container mx-auto h-screen  ">
+        <LabelText className="mb-8 text-gray-600 text-center py-2">What customers are saying</LabelText>
         <div className="flex flex-col md:flex-row md:-mx-3">
           {customerData.map(customer => (
             <div key={customer.customerName} className="flex-1 px-3">
@@ -135,15 +68,32 @@ export default () => (
           ))}
         </div>
       </div>
+      <div className="container mx-auto">
+        <LabelText className=" text-gray-600 text-center ">What customers are saying</LabelText>
+        <div className="flex flex-col md:flex-row md:-mx-3">
+          {customerData.map(customer => (
+            <div key={customer.customerName} className="flex-1 px-3">
+              <CustomerCard customer={customer} />
+            </div>
+          ))}
+        </div>
+      </div>
+      
     </section>
-    <section className="container mx-auto my-20 py-24 bg-gray-200 rounded-lg text-center">
-      <h3 className="text-5xl font-semibold">Ready to grow your business?</h3>
-      <p className="mt-8 text-xl font-light">
-        Quis lectus nulla at volutpat diam ut. Enim lobortis scelerisque fermentum dui faucibus in.
-      </p>
-      <p className="mt-8">
-        <Button size="xl">Get Started Now</Button>
-      </p>
+    <section id="retaltor" className="lg:h-screen  bg-yellow-300 pt-6" >
+      <div className="container mx-auto my-20 py-24 bg-gray-200 rounded-lg text-center">
+        <h3 className="text-5xl font-semibold">Ready to grow your business?</h3>
+        <p className="mt-8 text-xl font-light">
+          Quis lectus nulla at volutpat diam ut. Enim lobortis scelerisque fermentum dui faucibus in.
+        </p>
+      </div> 
+      <div className="container mx-auto my-20 py-24 bg-gray-200 rounded-lg text-center">
+        <h3 className="text-5xl font-semibold">Ready to grow your business?</h3>
+        <p className="mt-8 text-xl font-light">
+          Quis lectus nulla at volutpat diam ut. Enim lobortis scelerisque fermentum dui faucibus in.
+        </p>
+      </div> 
+      
     </section>
   </Layout>
 );
